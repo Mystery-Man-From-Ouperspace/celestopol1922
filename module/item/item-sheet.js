bbf4cb98b95b993fc33d8c9578df73e3f9f97047
Mystery-Man-From-Ouperspace <109296003+Mystery-Man-From-Ouperspace@users.noreply.github.com>
2024-03-24 09:58:55 +0100

    Add files via upload

---
+++ module/item/item-sheet.js
@@ -0,0 +1,49 @@
+import { CEL1922 } from "../config.js";
+/**
+ * @extends {ItemSheet}
+ */
+export class CEL1922ItemSheet extends ItemSheet {
+  /** @inheritdoc */
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["celestopol1922", "sheet", "item"],
+      template: "systems/celestopol1922/templates/item/item-sheet.html",
+      width: 520,
+      height: 520,
+      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }],
+      scrollY: [".description", ".attributes", ".notes"],
+    });
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  async getData(options) {
+    const context = await super.getData(options);
+    context.systemData = this.item.system;
+    context.subtypes = CEL1922.SUBTYPES
+    context.isWeapon = this.item.isWeapon;
+    context.isArmor = this.item.isArmor;
+    context.isVehicle = this.item.isVehicle;
+    context.isOther = this.item.isOther;
+    context.descriptionHTML = await TextEditor.enrichHTML(this.item.system.description, {
+      secrets: this.document.isOwner,
+      async: true,
+    });
+    context.notesHTML = await TextEditor.enrichHTML(this.item.system.notes, {
+      secrets: this.document.isOwner,
+      async: true,
+    });
+    return context;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritdoc */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Everything below here is only needed if the sheet is editable
+    if (!this.isEditable) return;
+  }
+}
